7add2e9047d9977ab4fdc9b7a7cf1160691fe78d
nshiff <nshiff@users.noreply.github.com>
2018-04-26 20:53:34 -0400

    display three random integers. represents the value on the spinning slot machine wheel.

--- main.c
+++ main.c
@@ -1,27 +1,29 @@
 /* 
 References
+https://stackoverflow.com/questions/9655202/how-to-convert-integer-to-string-in-c
 https://www.tutorialspoint.com/c_standard_library/c_function_rand.htm
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-int main(){
- 
-	int i, n;
-	time_t t;
-	n = 5;
-   
+void init(){
+    time_t t;
 	srand((unsigned) time(&t));
-
-	for( i = 0 ; i < n ; i++ ) {
-		printf("%d\n", rand() % 50);
-	}
-
-	return(0);
-
 }
 
+int main(){
+	init();
 
+    int NUM_WHEELS = 3;
+	int WHEELS[NUM_WHEELS];
 
+	for(int i = 0 ; i < NUM_WHEELS ; i++ ) {
+	    WHEELS[i] = rand() % 4;
+	}
+    printf("%d %d %d\n", WHEELS[0], WHEELS[1], WHEELS[2]);
+
+    return(0);
+}
