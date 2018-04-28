8b48a3e67c9e10989ecf7dd9068626e160ddd517
nshiff <nshiff@users.noreply.github.com>
2018-04-28 14:48:41 -0400

    ensure function can return all legal values

--- main.c
+++ main.c
@@ -30,14 +30,21 @@ int main(){
 // ---------------------------------------------------------------------------------------------------------------------
 
 void test_getPseudorandomIntegerForWheel(void){
+    srand(1234);
+    
     assert(2 == getPseudorandomIntegerForWheel());
     assert(3 == getPseudorandomIntegerForWheel());
     assert(3 == getPseudorandomIntegerForWheel());
+    assert(3 == getPseudorandomIntegerForWheel());
+
+    assert(0 == getPseudorandomIntegerForWheel());
+    assert(1 == getPseudorandomIntegerForWheel());
+    assert(3 == getPseudorandomIntegerForWheel());
+    assert(2 == getPseudorandomIntegerForWheel());
 }
 
 void runTestSuite(void){
     printf("running unit test suite...");
-    srand(1234);
     test_getPseudorandomIntegerForWheel();
     printf(" done\n");
 }
