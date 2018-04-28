1bda6574e427c923294887b19f06e4db658a02fa
nshiff <nshiff@users.noreply.github.com>
2018-04-28 13:23:41 -0400

    test nothing

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 /* 
 References
+https://www.slideshare.net/amritayan/test-driven-development-in-c
 https://stackoverflow.com/questions/9655202/how-to-convert-integer-to-string-in-c
 https://www.tutorialspoint.com/c_standard_library/c_function_rand.htm
 */
 
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-void init(){
+void test_nothing(void){
+    assert(false && "My first unit test");
+}
+
+void init(void){
     time_t t;
 	srand((unsigned) time(&t));
+	test_nothing();
 }
 
 int main(){
